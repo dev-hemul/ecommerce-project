1d24bc141d4f292e35a798c54f55771604d4a8b0
dev-hemul <dev.hemulll@gmail.com>
2026-01-17 05:51:21 +0200

    add cadd product

--- ecommerce-frontend/src/App.jsx
+++ ecommerce-frontend/src/App.jsx
@@ -1,22 +1,50 @@
-import { Routes, Route } from 'react-router'
-import { HomePage } from './pages/HomePage.jsx'
-import { CheckoutPage } from './pages/CheckoutPage.jsx'
-import { OrdersPage } from './pages/OrdersPage.jsx'
-import { TrackingPage } from './pages/TrackingPage.jsx'
-import { NotFoundPage } from './components/NotFoundPage.jsx'
+import axios from 'axios';
+import {Routes, Route} from 'react-router'
+import {useState, useEffect} from "react";
+import {HomePage} from './pages/HomePage.jsx'
+import {CheckoutPage} from './pages/CheckoutPage.jsx'
+import {OrdersPage} from './pages/OrdersPage.jsx'
+import {TrackingPage} from './pages/TrackingPage.jsx'
+import {NotFoundPage} from './components/NotFoundPage.jsx'
 import './App.css'
+
 
 function App() {
 
-  return (
-      <Routes>
-        <Route index element={<HomePage/>} />
-        <Route path="checkout" element={< CheckoutPage />} />
-        <Route path="orders" element={< OrdersPage />} />
-        <Route path="tracking" element={< TrackingPage />} />
+  const [cart, setCart] = useState([]);
 
-        <Route path="*" element={<NotFoundPage />} />
-      </Routes>
+  useEffect(() => {
+    axios.get('/api/cart-items?expand=product')
+      .then((response) => {
+        setCart(response.data);
+      })
+  }, [])
+
+
+  return (
+    <Routes>
+      <Route
+        index
+        element={<HomePage cart={cart}/>}
+      />
+      <Route
+        path="checkout"
+        element={< CheckoutPage cart={cart} />}
+      />
+      <Route
+        path="orders"
+        element={< OrdersPage />}
+      />
+      <Route
+        path="tracking"
+        element={< TrackingPage />}
+      />
+
+      <Route
+        path="*"
+        element={<NotFoundPage />}
+      />
+    </Routes>
   )
 }
 
